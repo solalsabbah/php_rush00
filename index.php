40e007bec267d96203ab1cd123e85c274b5fa160
Medno <phil.chadenier@gmail.com>
2018-03-31 01:42:01 +0200

    First push - Start of the front

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Site e-Commerce</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+<?php include('./static/header.php') ?>
+
+
+
+<?php include('./static/footer.html') ?>
+</body>
+</html>
---
+++ static/header.php
@@ -0,0 +1,44 @@
+<header>
+	<a href="http://192.168.99.100:8100/php_rush00/"><img id="logo" src="./img/logo.png"></a>
+	<nav>
+		<ul id="menu">
+			<li class="cat_1">
+				<a href="./categorie_1">CATEGORIE_1</a>
+				<ul class="sublist">
+					<li><a href="./categorie_1/sous_cat_1">sous_cat_1</a></li>
+					<li><a href="./categorie_1/sous_cat_2">sous_cat_</a></li>
+					<li><a href="./categorie_1/sous_cat_3">sous_cat_3</a></li>
+				</ul>
+			</li>
+			<li class="cat_1">
+				<a href="./categorie_2">CATEGORIE_2</a>
+				<ul class="sublist">
+					<li><a href="./categorie_1/sous_cat_1">sous_cat_1</a></li>
+					<li><a href="./categorie_1/sous_cat_2">sous_cat_2</a></li>
+					<li><a href="./categorie_1/sous_cat_3">sous_cat_3</a></li>
+				</ul>
+			</li>
+			<li class="cat_1">
+				<a href="./categorie_3">CATEGORIE_3</a>
+				<ul class="sublist">
+					<li><a href="./categorie_1/sous_cat_1">sous_cat_1</a></li>
+					<li><a href="./categorie_1/sous_cat_2">sous_cat_2</a></li>
+					<li><a href="./categorie_1/sous_cat_3">sous_cat_3</a></li>
+				</ul>
+			</li>
+			<li class="cat_1">
+				<a href="./categorie_4">CATEGORIE_4</a>
+			</li>
+			<li id="perso" class="ml_auto">
+				<a href="./account">Compte</a>
+				<ul class = "sublist">
+					<li><a href="./account/signin_page.php">Inscription</a></li>
+					<li><a href="./account/login.php">Connexion</a></li>
+				</ul>
+			</li>
+			<li id="perso" class="cat_1">
+				<a href="./panier">Panier</a>
+			</li>
+		</ul>
+	</nav>
+</header>
