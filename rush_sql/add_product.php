3c6c7db02edecf2772eb18712f24fcc35eb5b399
solalsabbah <solal.sabbah@gmail.com>
2018-04-01 00:19:12 +0200

    work on add product

--- rush_sql/add_product.php
+++ rush_sql/add_product.php
@@ -14,4 +14,12 @@ printf("Information sur le serveur : %s\n", mysqli_get_host_info($serv));
 
 VALUES(\"{$_SESSION['name']}\", \"{$_SESSION['price']}\", \"{$_SESSION['colour']}\",  \"{$_SESSION['quantity']}\", \"{$_SESSION['sexe']}\", \"{$_SESSION['path_img']}\")"; // add a new produt // replace _SESSION par $_SESSION[']
 
+
+$valide = 0
+while($valide = 0);
+{
+	if ($_SESSION['name'] && preg_match("^[0-9]([0-9]?){3}?$", $_SESSION['price']) && preg_match("^[0-9]([0-9]?){3}?$", $_SESSION['price']) && preg_match("^[a-zA-Z,]$", $_SESSION['quantity']) && preg_match("[MFA]", $_SESSION['sexe'])file_exists($_SESSION['path_img'])
+	$valide = 1;
+}
+
 ?>
